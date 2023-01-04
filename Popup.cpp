1c094686dbc24ef64020cc92fcd7c01ecafcf99c
J-Bros <mtaimoor0167@gmail.com>
2023-01-04 22:41:55 +0500

    Added mouse scrolls in Popup

--- Popup.cpp
+++ Popup.cpp
@@ -13,9 +13,23 @@ Popup::Popup(const string &filename) {
 void Popup::pop() {
     currentTopLine = 0;
     ChessWindow::popup = this;
+
+    bool scrolling = false;
+    int scroll{};
     int key{};
     do {
         key = GetKeyPressed();
+
+        scroll = GetMouseWheelMove();
+        if (scroll) {
+            if (!scrolling) {
+                key = scroll < 0 ? KEY_KP_2 : KEY_KP_8;
+                scrolling = true;
+            }
+        }
+        else
+            scrolling = false;
+
         switch (key) {
             case KEY_KP_8:
                 if (currentTopLine > 0)
